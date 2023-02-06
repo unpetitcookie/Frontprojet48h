90086fc515a6cd93362194d6b1ad355b99249ed4
JOUTIN <jules.outin@ynov.com>
2023-02-06 16:04:06 +0100

    Ajout de la partie react

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+import perso from'./perso.png';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <section>
+                <div class="header">
+                    <div class="header-left">
+                        <h1>Open-Octodon</h1>
+                    </div>
+                    <div class="header-right">
+                        <a class="active" href="#home">Home</a>
+                        <a class="login" href="">
+                        <img src={perso}  />
+                        </a>
+                    </div>
+                </div>
+            </section> 
+    )}
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import './home.css';
+import React from 'react';
+import './home.css';
+import Header from './Header.js';
+
+
+class App extends React.Component {  
+    render() {  
+      return (  
+        <div>
+           <Header /> 
+           <div class="home">
+                <div class="home-left">
+                    <div class="block">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      );  
+    }  
+  }  
+
+export default App; 
+
+
