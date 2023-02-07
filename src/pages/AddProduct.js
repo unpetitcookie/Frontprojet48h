7c56f7d332edc2c6c5efef826ebbebc2431d231b
unpetitcookie <matt.naert@gmail.com>
2023-02-07 12:10:46 +0100

    refactor react

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddProduct=()=>{
+   // const [image,setImage]=useState('');
+
+    const [description,setDescription]=useState('');
+    const [nameProduct,setNameProduct]=useState('');
+    const [price,setPrice]=useState('');
+    const [tag,setTag]=useState('');
+
+
+
+
+    const addProduct=(e)=>{
+        console.log(nameProduct,description,price,tag)
+        e.preventDefault();
+    }
     return(
         <div>
-                <form method="post" id ="formulaire">
+            <form  id ="formulaire" onSubmit={(e)=> addProduct(e)}>
          <div class="container">  
             <h1>Ajouter un Produit:</h1>
             <aside class="printimage">
@@ -14,16 +28,17 @@ const AddProduct=()=>{
                 
             </aside>
                 
-                    <input type="text" placeholder= "Nom du Produit" name="Produit" id ="nameProduct"></input><br/>
+                    <input value={nameProduct}onChange={(e)=>setNameProduct(e.target.value)} type="text" placeholder= "Nom du Produit" name="Produit" id ="nameProduct"></input><br/>
+
                 <div class="Descrition">
-                <textarea name="Descrition" placeholder="Descrition" rows="5" cols="30" id="Descrition"></textarea><br/>
+                <textarea value={description}onChange={(e)=>setDescription(e.target.value)} name="Descrition" placeholder="Descrition" rows="5" cols="30" id="Descrition"></textarea><br/>
                 </div>
                 <div>
                     <label for="productPrice">Prix :</label>
-                    <input type="number" placeholder="0" step="0.01" min="0"id="price"></input>
+                    <input value={price}onChange={(e)=>setPrice(e.target.value)} type="number" placeholder="0" step="0.01" min="0"id="price"></input>
                 </div>
                 <label>Tag:</label>
-                <input type="text" name="Tag" id="Tag"></input><br/>
+                <input value={tag}onChange={(e)=>setTag(e.target.value)} type="text" name="Tag" id="Tag"></input><br/>
                 
                 
                 <input type="submit" value="Submit" />
