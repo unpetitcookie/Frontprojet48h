fc97f53849d39aab0134b6b4a6dcc0a15a1b3146
fevan <evan.ferron53@gmail.com>
2023-02-07 10:21:35 +0100

    dzad

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,22 @@
+import React from 'react'
+
 const Login = () => {
     return (
         <div>
-            Hello it's me login page
+            <a href="Home.html"><img src="../image/image-home.png"/></a>
+            <div id="formContainer">
+                <h2>Se connecter</h2>
+                <form method="post" id="formulaire">
+                    <label for="Email">Email</label>
+                    <input required id="Email" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/><br/>
+                    <label for="mdp">Mot de passe</label>
+                    <input type="text" id="mdp"/><br/>
+                    <input type="submit" value="Log In" id="button" required/>
+                </form>
+            </div>
         </div>
-    )
-}
-
-export default Login;
+        
+        )
+    }
+    
+    export default Login;
